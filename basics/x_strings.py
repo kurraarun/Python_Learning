1c7a4ea03e47d00426833ca626ff83c3370e6175
kurraarun <kurraarun@gmail.com>
2022-01-20 22:17:04 +0530

    practicing of my python commands

---
+++ basics/x_strings.py
@@ -0,0 +1,85 @@
+# This excercise for different operations on strings
+
+string1 = 'Hello'
+
+string2 = ' '
+
+string3 = 'Moto'
+
+string4 = string1 + string2 + string3
+
+print(string4)
+
+print(string1 + string2 + string3)
+
+print('Hello' + ' ' + 'Moto')
+
+print()
+# Single Variable saving multiple lines 
+
+a = """Hello this Example 
+for Printing 
+Multiple Lines under One variable"""
+
+print(a)
+
+print('''Python is powerful language 
+and its simple 
+and its open source language ''')
+
+b = 'PYTHON'
+
+p,q,r,x,y,z = b
+
+print (p)
+print(q)
+print(r)
+print(x)
+print(y)
+print(z)
+
+concat_st = p+q+r+x+y+z
+
+print(concat_st)
+
+
+a = ['abc' ,'xyz','pqr','stg']
+
+print( a[0])
+
+print('hello \t sds')
+print()
+# Function to identify the starting or ending string
+
+a ="Hello World!"
+
+print(a.startswith('hello'))
+print(a.endswith('World!'))
+print()
+
+# Split the string and assign to different variable
+
+a="Arun Kumar Kurra"
+print(a.split(" "))
+
+b = a.split(" ")
+print(b)
+
+a ="Helloaworldaworldisabigaenough"
+
+print(a.split("a"))
+
+
+challenge = ' thirty days of python '
+print(challenge.strip('y')) # 5
+print()
+
+# Swap the each letter in the given string from Lower case to Upper and Upper case to Lower
+str1 = 'hello world'
+print(str1.swapcase())
+
+str2 = 'HELLO WORLD'
+print(str2.swapcase())
+
+str3 = 'Hello World'
+print(str3.swapcase())
---
+++ basics/x_variable_dec.py
@@ -0,0 +1,35 @@
+# Program meant for Variable declarations
+
+
+# Declaring the variable
+
+var1 = 'Hello'
+var2 = 322
+var3 = 23.6432
+var4 = ['World' ,22,54.23,'Young']
+var5 = True
+
+var6 = {'Welcome to ': ' Python World',' This Program is  ':' Simple and effective '} 
+
+# printing the variables
+
+print()
+print (var1,var2,var3)
+
+print (var4)
+print (var5)
+print (var6)
+
+c = 'Kurra'
+
+var7 = ['hello ', 'World ' ,'Welcome ']
+
+var8 = ('hello ', 'World ' ,'Welcome ')
+
+var9 = (1,3,2,5,6,67)
+
+
+print (var7 )
+print (var8 )
+print (var9 )
+
